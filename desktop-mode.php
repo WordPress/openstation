058b8e4adfda06cec8511a4ad086f8ae4af4fc11
Roberto Aranda <roberto.aranda@automattic.com>
2026-09-24 20:16:27 +0200

    Plugin header: credit WordPress.org as the author
    The wordpress.org directory reads the "By" byline from the Author header of the main plugin file. Point it (and Author URI) at WordPress.org so the plugin page shows the project rather than an individual. Takes effect on the next release.

--- desktop-mode.php
+++ desktop-mode.php
@@ -6,8 +6,8 @@
  * Version:           1.1.11
  * Requires at least: 6.0
  * Requires PHP:      7.4
- * Author:            Daniel López Sánchez
- * Author URI:        https://github.com/allterraindeveloper
+ * Author:            WordPress.org
+ * Author URI:        https://wordpress.org/
  * License:           GPLv2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       desktop-mode
